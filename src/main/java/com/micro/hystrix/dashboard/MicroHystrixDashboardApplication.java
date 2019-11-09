afb730a689a525019c456ea33792a7fd48491641
Polin <853450031@qq.com>
2019-11-09 20:34:54 +0800

    update

--- src/main/java/com/micro/hystrix/dashboard/MicroHystrixDashboardApplication.java
+++ src/main/java/com/micro/hystrix/dashboard/MicroHystrixDashboardApplication.java
@@ -5,53 +5,33 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
-
 /**
- * Hello world!
- *http://127.0.0.1:8086/hystrix
+ * Hello world! http://127.0.0.1:8086/hystrix
  */
 @EnableEurekaClient
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableCircuitBreaker
-@EnableHystrix
-public class MicroHystrixDashboardApplication
-{
+public class MicroHystrixDashboardApplication {
 	public static void main(String[] args) {
-        SpringApplication.run(MicroHystrixDashboardApplication.class, args);
-    }
+		ApplicationContext ctx = SpringApplication.run(MicroHystrixDashboardApplication.class, args);
+	}
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+		};
+	}
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
-    
-    @Bean
-    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet(){
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }
 }
